a0c892040ea0e30f338a1892c25f019d12dc5088
MishaHryvinskyi <116170668+MishaHryvinskyi@users.noreply.github.com>
2023-04-21 20:39:39 +0000

    Deploying to gh-pages from @ MishaHryvinskyi/goit-js-hw-11@51814b6638f0ffcb95c04259a6ca3726b650f6f0 🚀

--- index.1b40902c.js
+++
@@ -1,2 +0,0 @@
-(document.querySelector(".search-form"),document.querySelector("input"),document.querySelector(".gallery"),document.querySelector('button[type="submit"]')).addEventListener("click",(function(){console.log("Ти клікнув на кнопку")}));
-//# sourceMappingURL=index.1b40902c.js.map
--- index.f667fbc8.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector(".search-form"),input:document.querySelector("input"),gallery:document.querySelector(".gallery"),btnSearch:document.querySelector('button[type="submit"]')}).btnSearch.addEventListener("click",(()=>{console.log("Ти клікнув на кнопку")}));
-//# sourceMappingURL=index.f667fbc8.js.map
